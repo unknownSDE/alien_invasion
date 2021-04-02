0d2250f77149026c13c7a64e420078815dcf3af3
unknownSDE <hs.thakur107@gmail.com>
2021-04-03 00:12:27 +0530

    Created base class and added image on screen

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+	'''A class to store all the settings for the game alien invasion.'''
+
+	def __init__(self):
+		"""initialize the game's settings."""
+
+		#screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230,230,230)
