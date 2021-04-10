18a3b65ce935550c4d34ced04fb07c51fed4242a
unknownSDE <hs.thakur107@gmail.com>
2021-04-10 16:03:19 +0530

    Added bullets and fire bullets. End of first module of the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -19,6 +20,7 @@ class AlienInvasion:
 		pygame.display.set_caption( "Alien Invasion")
 
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 
 	def run_game(self):
@@ -27,10 +29,19 @@ class AlienInvasion:
 			#watch for keyboard and mouse events.
 			self._check_events()
 			self.ship.update()
-			#redraw the screen during each pass through the loop.
+			self._update_bullets()
 			self._update_screen()
 
-			
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets"""
+		#update bullet positions.
+		self.bullets.update()
+
+		#get rid of bullets that have disappeared.
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+		#print(len(self.bullets))		
 
 	def _check_events(self):
 		'''Respond to keyboard and mouse events.'''
@@ -52,6 +63,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		'''responds to key releases'''
@@ -60,11 +73,19 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		"""create a new bullet and add it to the bullets group."""
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+
 
 	def _update_screen(self):
 		'''Update images on the screen, and flip to the new screen.'''
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 		#make the most recently drawn screeen visible.
 		pygame.display.flip()
